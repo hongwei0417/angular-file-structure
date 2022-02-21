60a214d5d34d1ade3ef9f61339fff4d0aa1e1a8f
Kevin Hu <iamhongwei0417@gmail.com>
2022-02-21 14:12:07 +0800

    update name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'shelf-management'`, () => {
+  it(`should have as title 'angular-file-structure'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('shelf-management');
+    expect(app.title).toEqual('angular-file-structure');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('shelf-management app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'angular-file-structure app is running!'
+    );
   });
 });
